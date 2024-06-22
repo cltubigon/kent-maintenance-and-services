d0e7192622678a9361636831b70ec67cf6e6ff9c
cltubigon <cltubigon@gmail.com>
2024-06-23 04:44:40 +0800

    fixed minor issue of the previous commit4

--- src/app/contact-us/CuSection1a.js
+++ src/app/contact-us/CuSection1a.js
@@ -66,7 +66,7 @@ const CuSection1a = () => {
             <p className={''}>Medway</p>
           </div>
         </div>
-        <p className={'mt-5 text-center bg-secondary-100 p-3 rounded-md shadow-sm border-2 border-secondary-300'}>
+        <p className={'mt-5 text-center bg-neutral-200 p-3 rounded-md shadow-sm border-2 border-neutral-400 textwh'}>
           Most areas in Kent. Depending on location there may be additional
           travel costs but this will be confirmed in any quotes.
         </p>
